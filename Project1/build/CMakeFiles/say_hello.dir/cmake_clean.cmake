2f2f9a09b01fe646da2ee1205b433390c439dfeb
root <ganesh.arvapalli@gmail.com>
2020-01-31 13:59:36 +0000

    First commit in Project 1

---
+++ Project1/build/CMakeFiles/say_hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/say_hello.dir/script/say_hello.cpp.o"
+  "libsay_hello.pdb"
+  "libsay_hello.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/say_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
